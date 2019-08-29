d81b13d4974803ff04dbe7d9fa69394a969a3353
Lucas <lucas.lopes5@hotmail.com>
2019-08-29 09:21:43 -0300

    Alterando CSS

--- load.js
+++ load.js
@@ -1,36 +1,34 @@
-function createNode(element)
-{
-    return document.createElement(element);
+const app = document.getElementById('root')
+
+const container = document.getElementById('div')
+container.setAttribute('container')
+
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'http://127.0.0.1:8081/assets/tasks.json', true)
+request.onload = function() {
+
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(list => {
+      const label = document.getElementById('title')
+      p.textContent = `${movie.title}...`
+
+
+      const p = document.getElementById('p')
+      p.textContent = list.title
+
+
+      container.appendChild(title)
+      div.appendChild(label)
+      div.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
 }
 
-function append(parent, el)
-{
-    return parent.appendChild(el);
-}
-
-const label = document.getElementById("title");
-
-const url = "../assets/tasks.json";
-
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
-      let title = data.results;
-
-      return title.map(function(lista) {
-
-        let label = createNode('label');
-
-        lable.innerHTML=`${lista.title}`;
-
-        append(label);
-
-      })
-
-})
-
-.catch(function(error) {
-
-      console.log(JSON.stringify(error));
-
-})
+request.send()
